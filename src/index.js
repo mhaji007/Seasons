0798cbbf60c70bc1e5feef5a201d9dd17021d8a0
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-02-21 22:21:37 -0500

    refactor state initialization

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {lat: null, errorMessage: ''};
-    }
+    state = {lat: null, errorMessage: ''};
 
     componentDidMount () {
 
